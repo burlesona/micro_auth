c193e1ab55c70d78e1e95444d1823909e60eaf8e
Andrew Burleson <andrew@eighty-b.com>
2012-06-11 12:30:33 -0500

    Removed unnecessary comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  #match "/#{HighVoltage.content_path}*id" => 'high_voltage/pages#show', :as => :page, :format => false
   get 'login' => 'micro_auth/sessions#new', :as => 'login'
   get 'logout' => 'micro_auth/sessions#destroy', :as => 'logout'
   namespace :micro_auth do
